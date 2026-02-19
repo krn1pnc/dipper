1629d213c8c51bac80dfff1845a471cfc16087a1
krn1pnc <krn1pnc@outlook.com>
2026-02-19 15:38:27 +0800

    feat(array2d): better boundary check

--- array2d/array2d.rs
+++ array2d/array2d.rs
@@ -26,12 +26,12 @@ impl<T> Array2d<T> {
         return Self::new((M, N), d.into_iter().flat_map(|x| x.into_iter()).collect());
     }
 
-    pub fn get(&self, i: usize, j: usize) -> &T {
-        return self.data.get(i * self.shape.1 + j).expect("out of bound");
+    pub fn get(&self, i: usize, j: usize) -> Option<&T> {
+        return self.data.get(i * self.shape.1 + j);
     }
 
-    pub fn get_mut(&mut self, i: usize, j: usize) -> &mut T {
-        return self.data.get_mut(i * self.shape.1 + j).expect("out of bound");
+    pub fn get_mut(&mut self, i: usize, j: usize) -> Option<&mut T> {
+        return self.data.get_mut(i * self.shape.1 + j);
     }
 }
 
@@ -39,12 +39,12 @@ impl<T> Index<(usize, usize)> for Array2d<T> {
     type Output = T;
 
     fn index(&self, index: (usize, usize)) -> &Self::Output {
-        return self.get(index.0, index.1);
+        return self.get(index.0, index.1).expect("out of bound");
     }
 }
 
 impl<T> IndexMut<(usize, usize)> for Array2d<T> {
     fn index_mut(&mut self, index: (usize, usize)) -> &mut Self::Output {
-        return self.get_mut(index.0, index.1);
+        return self.get_mut(index.0, index.1).expect("out of bound");
     }
 }
